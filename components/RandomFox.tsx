d5a8034bfe4a38707bcefaf8fe1cb355b800667b
IvanG <kikin-gr2000@hotmail.com>
2023-04-20 22:21:19 -0600

    end of the class 10

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,12 +1,55 @@
 import React from 'react'
 
-// generate a random function between 1 and 123
-const randomNumber = () => Math.floor(Math.random() * 123) + 1;
+type Props = {
+  image: string,
+  alt: string
+};
 
-const RandomFox = (): JSX.Element => {
-  const image = `https://randomfox.ca/images/${randomNumber()}.jpg`;
+let options = {
+  root: null, // debe apuntar al parent del objeto a observar
+  rootMargin: '0px',
+  threshold: 1, // si es 1 es hasta que el objeto se muestre al 100%, o 0.5 si se muestra la mitad
+}
+
+const RandomFox = ({ image, alt }: Props): JSX.Element => {
+  const node = React.useRef<HTMLImageElement>(null);
+  const [ src, setSrc ] = React.useState<string>("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=");
+
+  // aseguramos que pase solo en el lado del cliente con useEffect
+  // con el [] aseguramos que solo se ejecute cuando se monte el componente
+  // es decir, que ya existe una referencia a una imagen que sea observable
+
+  React.useEffect(() => {
+    // nuevo observador
+    let observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setSrc(image);
+        }
+      })
+    }/*, options*/);
+
+    // observe node
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    // desconectar observer
+    return () => {
+      observer.disconnect();
+    }
+  }, [ image ])
+
+
   return (
-    <img width={320} height='auto' src={image} className='rounded' />
+    <img
+      ref={node}
+      width={320}
+      height='auto'
+      src={src}
+      className='rounded'
+      alt={alt}
+    />
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,58 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { RandomFox } from '@/components/RandomFox'
+import React from 'react';
 
 const inter = Inter({ subsets: [ 'latin' ] })
 
+// generate a random function between 1 and 123
+const randomNumber = () => Math.floor(Math.random() * 123) + 1;
+// generate unique id's from a function
+const generateId = () => Math.random().toString(36).substring(2, 9);
+
+type ImageItem = {
+  id: string,
+  url: string
+};
+
 export default function Home() {
+
+  const [ images, setImages ] = React.useState<ImageItem[]>([]);
+
+  const addNewFox: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+
+    const newImageItem: ImageItem = {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${randomNumber()}.jpg`
+    }
+    setImages([
+      ...images,
+      newImageItem
+    ]);
+  }
+
   return (
-    <div>
+    <>
       {/* <head>
         <title>Platzi</title>
       </head> */}
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className={`${inter.className}`}>
-          <h1 className='text-3xl font-bold underline'>Hello Platzi</h1>
-          <RandomFox />
-        </div>
-      </main>
-    </div>
+      <div>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <div >
+            <h1 className={`text-4xl font-bold underline ${inter.className}`}>Hello Platzi</h1>
+            <button onClick={addNewFox}>Add new fox</button>
+            {
+              images.map((image) => {
+                return (
+                  <div key={image.id} className="p-4">
+                    <RandomFox alt="dasd" image={image.url} />
+                  </div>
+                );
+              })
+            }
+          </div>
+        </main>
+      </div>
+    </>
   )
 }
